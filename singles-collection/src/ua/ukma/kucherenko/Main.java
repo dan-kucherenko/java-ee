03f8a8fbb9cdacdfaa124cebbfa17646992eed14
dan-kucherenko <daniil.ukraine@gmail.com>
2024-04-10 23:14:22 +0300

    Correct the Singleton practice

--- singles-collection/src/ua/ukma/kucherenko/Main.java
+++ singles-collection/src/ua/ukma/kucherenko/Main.java
@@ -1,30 +1,35 @@
 package ua.ukma.kucherenko;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException {
         System.out.println("Single call of foo()");
         // run foo() from static instance in singleton class
         SingletonClass.getInstance().foo();
 
         System.out.println("\nCalling foo() 5 times using reflection");
         List<SingletonClass> singletonList = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            SingletonClass instance = SingletonClass.getInstance();
-            singletonList.add(instance);
+        try {
+            Constructor<SingletonClass> constructor = SingletonClass.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+
+            for (int i = 0; i < 5; i++) {
+                SingletonClass singletonObj = constructor.newInstance();
+                singletonList.add(singletonObj);
+            }
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
         }
 
         for (SingletonClass instance : singletonList) {
-            try {
-                // reflection call of the foo method
-                Method method = instance.getClass().getMethod("foo");
-                method.invoke(instance);
-            } catch (ReflectiveOperationException e) {
-                System.err.println("Error invoking foo method: " + e.getMessage());
-            }
+            // reflection call of the foo method
+            System.out.println(instance.hashCode());
+            instance.foo();
         }
     }
 }
